ba1245b726c69ff39152306307f55fb7ce37c32c
Johnny <108742568+superyyds@users.noreply.github.com>
2026-01-11 14:06:21 +0800

    Add files via upload

---
+++ secret.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// WiFi
+static const char* WIFI_SSID     = "Peeee";
+static const char* WIFI_PASSWORD = "244466666";
+
+// MQTT
+static const char* MQTT_SERVER   = "34.29.181.85";
+static const char* DEVICE_ID     = "makerfeathers301";
+
+// Telegram
+#define BOT_TOKEN  "8153754842:AAHWcBv9ng1qQT4R0_dEeL1kQNaycGcQ7kE"
+#define CHAT_ID    "-1003408639046"
